891e5ad34d418be85b406732f8e3933c636ca05d
procodershubh <shubhamkumar84545556@gmail.com>
2024-09-25 16:56:29 +0530

    ass

--- src/Servicepage.js
+++ src/Servicepage.js
@@ -3,14 +3,14 @@
 
 // //local server
 
-export const backendurl = "http://localhost:5782";
+// export const backendurl = "http://localhost:5782";
 
 
 
 // //live server
 
 
-// export const backendurl = "https://ecommercebackend-cuex.onrender.com";
+export const backendurl = "https://ecommercebackend-cuex.onrender.com";
 
 
 
